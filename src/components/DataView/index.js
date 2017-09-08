731f158434faf89606798df96d2ec7079e9cea8c
Ariel Gerstein <ariel.ger@hotmail.com>
2017-09-08 02:30:29 -0300

    Add styles for list of subjects

--- src/components/DataView/index.js
+++ src/components/DataView/index.js
@@ -14,31 +14,32 @@ class DataView extends Component {
     const { query } = this.props;
 
     return (
-      <div>
-        <pre>{JSON.stringify(query)}</pre>
+      <div className="data-section">
         {data
           .filter(subject => query.subjects.includes(subject.code))
           .map(subject =>
-            <div>
-              <h1>{subject.code}.{subject.name}</h1>
+            <div className="subject">
+              <h1 className="subject__title">
+                <span className="subject__title__code">{subject.code}</span>
+                <span className="subject__title__text">{subject.name}</span>
+              </h1>
               {
                 subject.chairs.map(chair =>
-                  <div>
-                    <h2>{chair.name}</h2>
+                  <div className="chair">
+                    <h2 className="chair__title">
+                      <span className="chair__title__pre">Catedra </span>
+                      {chair.name}
+                    </h2>
                     { chair.places
                       .filter(place => query.places.includes(place.name))
                       .map(place =>
-                        <div>
-                          <h3>{place.name}</h3>
-                          { place.items.map(item =>
-                              <div>
-                                <h4>Codigo: {item.code}</h4>
-                                <h5>Profesor: {item.professor}</h5>
-                                <h5>Mode: {item.mode}</h5>
-                              </div>
-                            )
-                          }
-                        </div>
+                        place.items.map(item =>
+                          <div className="item">
+                            <span className="item__code">{item.code}</span>
+                            <h5>Profesor: {item.professor}</h5>
+                            <h5>Mode: {item.mode}</h5>
+                          </div>
+                        )
                       )
                     }
                   </div>
@@ -47,6 +48,7 @@ class DataView extends Component {
             </div>
           )
         }
+        <pre>{JSON.stringify(query)}</pre>
       </div>
     );
   }
